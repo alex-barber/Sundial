a90c0e50b18441eb66b6bcce2740dfe536099144
Alex Barber <alexanderbdev@gmail.com>
2019-07-26 00:50:05 -0500

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Hello } from './components/Hello';
+
+declare let module: any // for hotreloader
+
+ReactDOM.render(
+  <Hello compiler="Typescript" framework="React" bundler="Webpack" />,
+  document.getElementById('root')
+);
+
+
+if (module.hot) {
+   module.hot.accept(); // for hotreloader
+}
