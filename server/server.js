e42966bbf6cbd31f1d95871887e1e35b1d401557
Alex Barber <alexanderbdev@gmail.com>
2020-06-13 19:10:03 -0500

    prettier

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require('path'),
-   express = require('express'),
-   webpack = require('webpack'),
-   webpackConfig = require('../webpack.config.js'),
-   app = express(),
-   port = process.env.PORT || 3000;
-app.listen(port, () => { console.log(`App is listening on port ${port}`) });
+      express = require('express'),
+      webpack = require('webpack'),
+      webpackConfig = require('../webpack.config.js'),
+
+      app = express(),
+      port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
 app.get('/', (req, res) => {
-   res.sendFile(path.resolve(__dirname,'..', 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 let compiler = webpack(webpackConfig);
-app.use(require('webpack-dev-middleware')(compiler, {
-   noInfo: true, publicPath: webpackConfig.output.publicPath, stats:    { colors: true }
-}));
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    stats: { colors: true },
+  })
+);
 app.use(require('webpack-hot-middleware')(compiler));
-app.use(express.static(path.resolve(__dirname,'..', 'public')));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
